bbfff23aaa7bf392fff1402f3885af89986f7fc0
deathchurch <37252590+deathchurch@users.noreply.github.com>
2019-05-11 15:46:26 +0100

    Update install-docker.ps1

--- install-docker.ps1
+++ install-docker.ps1
@@ -1,3 +1,4 @@
 # Install Docker
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider
+(Install-WindowsFeature Containers).RestartNeeded
